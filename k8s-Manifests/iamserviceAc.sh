0f9586ee9ee2b3ce4f2a34f41eb69fd075978159
EC2 Default User <ec2-user@ip-172-31-6-22.ec2.internal>
2022-08-14 18:26:39 +0000

    working on images

--- k8s-Manifests/iamserviceAc.sh
+++ k8s-Manifests/iamserviceAc.sh
@@ -1,3 +1,2 @@
 eksctl utils associate-iam-oidc-provider --cluster clo835 --approve
 eksctl create iamserviceaccount     --name clo835     --namespace final     --cluster clo835     --attach-policy-arn arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess     --approve     --override-existing-serviceaccounts
-
