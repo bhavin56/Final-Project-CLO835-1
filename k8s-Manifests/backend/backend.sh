ccd0a061e99a2553df98a638c29a273e794fd25e
EC2 Default User <ec2-user@ip-172-31-6-210.ec2.internal>
2022-08-14 21:17:52 +0000

    Refactored the html file

--- k8s-Manifests/backend/backend.sh
+++ k8s-Manifests/backend/backend.sh
@@ -1,5 +1,7 @@
 k create -f secret-for-sqldb.yaml -n final
 k create -f pvc-for-sqldb.yaml -n final
+k get pvc -n final
 k create -f sqldb-pod-deployment.yaml -n final
 k create -f service-sqldb.yaml -n final
 k get all -n final
+k get pvc -n final
